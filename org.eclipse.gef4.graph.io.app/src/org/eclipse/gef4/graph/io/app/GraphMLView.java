21c316c86f466d25fba5cbea564f419016f7564f
lucas bigeardel <lucas.bigeardel@gmail.com>
2015-12-28 12:14:21 -0800

    refactor

--- org.eclipse.gef4.graph.io.app/src/org/eclipse/gef4/graph/io/app/GraphMLView.java
+++ org.eclipse.gef4.graph.io.app/src/org/eclipse/gef4/graph/io/app/GraphMLView.java
@@ -5,7 +5,6 @@ import java.io.File;
 import org.eclipse.gef4.graph.Graph;
 import org.eclipse.gef4.graph.io.graphml.GraphMLParser;
 import org.eclipse.gef4.layout.algorithms.SpringLayoutAlgorithm;
-import org.eclipse.gef4.zest.examples.graph.ZestGraphExample;
 import org.eclipse.gef4.zest.fx.ZestProperties;
 import org.eclipse.gef4.zest.fx.ui.parts.ZestFxUiView;
 import org.eclipse.jface.action.Action;
@@ -15,9 +14,11 @@ import org.eclipse.swt.widgets.FileDialog;
 
 public class GraphMLView extends ZestFxUiView {
 	public GraphMLView() {
-		
-		setGraph(ZestGraphExample.createDefaultGraph());
-	}
+		GraphMLParser nestedGraphMLParser = new GraphMLParser(new File("samples\\simple.xml"));
+		Graph graph = nestedGraphMLParser.load();
+		graph.getAttrs().put(ZestProperties.GRAPH_TYPE, ZestProperties.GRAPH_TYPE_DIRECTED);
+		graph.getAttrs().put(ZestProperties.GRAPH_LAYOUT_ALGORITHM, new SpringLayoutAlgorithm());
+		setGraph(graph);	}
 	
 	@Override
 	public void createPartControl(Composite parent) {
--- org.eclipse.gef4.graph.io.tests/src/org/eclipse/gef4/graph/io/tests/ProdConsPartitionLayoutExample.java
+++ org.eclipse.gef4.graph.io.tests/src/org/eclipse/gef4/graph/io/tests/ProdConsPartitionLayoutExample.java
@@ -1,4 +1,4 @@
-package org.eclipse.gef4.graph.io.app;
+package org.eclipse.gef4.graph.io.tests;
 /*******************************************************************************
  * Copyright (c) 2014, 2015 itemis AG and others.
  *
